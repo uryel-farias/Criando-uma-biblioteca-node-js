c4f22d0e499e869b44ea2cb056d08933d92842d7
Uryel Farias <uryel.farias@gmail.com>
2025-05-22 10:22:59 -0300

    convertendo o arquivo para o topo texto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const caminhoArquivo = process.argv;
+const fs = require('fs');
 
+const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]; 
 
-console.log(link);
+fs.readFile(link, 'utf-8', (erro, texto) => {
+  console.log(texto);
+})
