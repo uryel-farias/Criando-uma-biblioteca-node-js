19ea4e749088995cb7ecc2f7527c647741e6df46
Uryel Farias <uryel.farias@gmail.com>
2025-05-23 10:35:59 -0300

    verificando palavras duplicadas e quebras de paragrafo

--- src/index.js
+++ src/index.js
@@ -16,3 +16,21 @@ function verificaPalavrasDuplicadas(texto) {
   })
   console.log(resultado);
 }
+
+function quebraEmParagrafos(texto) {
+  const paragrafos = texto.toLowerCase().split('\n');
+  const contagem = paragrafos.map((paragrafo) => {
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
+  console.log(contagem);
+}
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  // objeto[propriedade] = valor;
+  listaPalavras.forEach(palavra => {
+    resultado[palavra] = (resultado[palavra] || 0) + 1
+  })
+  return resultado;
+}
