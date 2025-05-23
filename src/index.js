d39b81561be71bd1c80db488a8825b734a93de55
Uryel Farias <uryel.farias@gmail.com>
2025-05-23 10:42:48 -0300

    removendo codigo duplicado

--- src/index.js
+++ src/index.js
@@ -4,18 +4,8 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]; 
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-  verificaPalavrasDuplicadas(texto);
+  quebraEmParagrafos(texto)
 })
-
-function verificaPalavrasDuplicadas(texto) {
-  const listaPalavras = texto.split(' ');
-  const resultado = {};
-  // objeto[propriedade] = valor;
-  listaPalavras.forEach(palavra => {
-    resultado[palavra] = (resultado[palavra] || 0) + 1
-  })
-  console.log(resultado);
-}
 
 function quebraEmParagrafos(texto) {
   const paragrafos = texto.toLowerCase().split('\n');
