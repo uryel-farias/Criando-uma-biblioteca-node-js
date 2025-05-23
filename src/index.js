ba383abe33c1dff81fc17d009efb5e07090b7fd1
Uryel Farias <uryel.farias@gmail.com>
2025-05-23 11:03:18 -0300

    correção da palavra length na função verificaPalavraDuplicada

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const fs = require('fs');
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]; 
 
+// Pagando um arquivo de texto e quebrando ele em paragrafos
+// =========================================================
+
 fs.readFile(link, 'utf-8', (erro, texto) => {
   quebraEmParagrafos(texto)
 })
+
+// função que faz a quebra do texto em paragrafos e ao final vericica as palavras duplicadas
+// =========================================================================================
 
 function quebraEmParagrafos(texto) {
   const paragrafos = texto.toLowerCase().split('\n');
@@ -15,12 +21,24 @@ function quebraEmParagrafos(texto) {
   console.log(contagem);
 }
 
+// função que vericica as palavras duplicadas
+// ==========================================
+
 function verificaPalavrasDuplicadas(texto) {
   const listaPalavras = texto.split(' ');
   const resultado = {};
   // objeto[propriedade] = valor;
   listaPalavras.forEach(palavra => {
-    resultado[palavra] = (resultado[palavra] || 0) + 1
+    if(palavra.length >= 3) {
+        const palavrasLimpa = limpaPalavras(palavra);
+        resultado[palavrasLimpa] = (resultado[palavrasLimpa] || 0) + 1;
+    }
   })
   return resultado;
+}
+
+
+
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
 }
