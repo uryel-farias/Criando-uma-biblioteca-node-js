341e7bf8fa51b671849383220c884a2ba423be5c
Uryel Farias <uryel.farias@gmail.com>
2025-05-23 10:24:41 -0300

    Separando palavras por palavras em um objeto

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]; 
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-  console.log(texto);
+  verificaPalavrasDuplicadas(texto);
 })
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  // objeto[propriedade] = valor;
+  listaPalavras.forEach(palavra => {
+    resultado[palavra] = (resultado[palavra] || 0) + 1
+  })
+  console.log(resultado);
+}
